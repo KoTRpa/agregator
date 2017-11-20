8bea5dea9f983b12d93c1310391534e715b88713
KoTRpa <4kotrpa@gmail.com>
2017-11-20 23:42:49 +0800

    add seed for admin user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+            'name' => 'Admin',
+            'email' => '4kotrpa@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+    }
+}
