0e8827e9b15d4df9151dbabc5f90839df708a39f
KoTRpa <4kotrpa@gmail.com>
2017-11-20 21:55:15 +0800

    some fixes to react

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
-    </head>
-    <body>
+@section('content')
         <h2 style="text-align: center"> Laravel and React application </h2>
         <div id="root"></div>
-        <script src="{{ mix('js/app.js') }}" ></script>
-    </body>
-</html>
+@endsection
