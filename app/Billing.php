97a205deea8d22dea1038a47e762f9bbf38471ce
KoTRpa <4kotrpa@gmail.com>
2017-11-20 22:58:12 +0800

    basic migrations for payments, billings, categories and tags tables with basic models

--- app/Billing.php
+++ app/Billing.php
@@ -3,18 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
 
-class Payment extends Model
+class Billing extends Model
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'user_id', 'amount', 'direction', 'billing_id', 'category_id', 'comment'
+        'title',
     ];
 }
