8539b8bb8c96fdb40baa1a2895a9347ee8b1079c
KoTRpa <4kotrpa@gmail.com>
2017-11-20 22:04:00 +0800

    payments model boilerplate

---
+++ app/Payment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Payment extends Model
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'amount', 'user_id', 'positive', 'service', 'category_id'
+    ];
+}
